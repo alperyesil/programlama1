1909f2bc54b86f2beecf9ebb84f59f4430707c02
Alper Yeşil <36932084+alperyesil@users.noreply.github.com>
2018-10-30 15:49:34 +0300

    Delete soru3.py

--- soru3.py
+++
@@ -1,6 +0,0 @@
-import soru3modul
-etkilesim_orani=0
-grup=0
-ybs1=soru3modul.engagement_rate(365000,65000,870,500,1125000)
-ybs2=soru3modul.engagement_rate(450000,25000,380,100,1450000)
-ybs3=soru3modul.engagement_rate(582000,52000,1200,650,2000000)
